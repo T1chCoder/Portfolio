230e0825aa895b373102591b9a5bb339480ff113
Asadbek <zhumabaevasadbek123@gmail.com>
2025-01-08 06:48:31 +0500

    header fix

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,2 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    $("#nvbr-chckbx").change(function () {
+        if (this.checked && $("#nvbr").hasClass("anmt")) {
+            $("#nvbr").removeClass("anmt");
+        }
+        else if (!this.checked && !$("#nvbr").hasClass("anmt")) {
+            $("#nvbr").addClass("anmt");
+            setTimeout(function () {
+                $("#nvbr").removeClass("anmt");
+            }, 350);
+        }
+    });
 });
